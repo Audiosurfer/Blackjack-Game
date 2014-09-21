9fb82f5a933a67625b434e9667059426c002bf30
Audiosurfer <gabolad@hotmail.com>
2014-09-20 19:40:49 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    var totalAmount = 0;
+    var compAmount = 0;
+    var compGen = function() {
+        compAmount = (Math.floor(Math.random() * 11) * 2) + 1;
+        while (compAmount <= 10) {
+            compAmount += Math.ceil(Math.random() * 11);
+        }
+    };
+    
+    var compareScore = function() {
+        if (totalAmount === 21) {
+            $('#results').text("Blackjack! You win!");
+        }
+        else if (totalAmount > 21) {
+            $('#results').text('You busted!');
+        }
+        else if (totalAmount > compAmount) {
+            $('#results').text("Congrats! You win!");
+        }
+        else if (totalAmount < compAmount) {
+            $('#results').text("I'm sorry, looks like you lose.")
+        }
+        else {
+            $('#results').text("It's a tie. How boring.")
+        }
+    };
+    
+    var restart = function() {
+        $('#draw').show();
+        $('#results').hide();
+        $('#userscore').text(0);
+        $('#compscore').text("?");
+        totalAmount = 0;
+        compAmount = 0;
+    };
+                                                  
+    $('#yes').click(function() {
+        var drawAmount = Math.ceil(Math.random() * 11)
+        totalAmount += drawAmount;
+        $('#userscore').text(totalAmount);
+    })
+    
+    $('#no').click(function() {
+        compGen();
+        $('#compscore').text(compAmount);
+        $('#draw').hide();
+        $('#results').show();
+        compareScore();
+    })
+    
+    $('#newgame').click(function() {
+        restart();
+    })
+});
